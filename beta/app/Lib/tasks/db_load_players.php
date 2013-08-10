641b542f4a0375d46675465ce6b3587ffe7bef2f
Matt Dahl <matt.dahl.2013@gmail.com>
2013-08-10 01:32:59 -0400

    Updates php scripts for loading village/player data. Now decodes.

--- beta/app/Lib/tasks/db_load_players.php
+++ beta/app/Lib/tasks/db_load_players.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Intended for command line usage for local development. Loads village data into the local database. Example syntax for world 70:
+ * Intended for command line usage for local development. Loads player data into the local database. Example syntax for world 70:
  *
- * $ php db_load_villages.php 70
+ * $ php db_load_players.php 70
  */
 
 // Tracks execution time
@@ -12,27 +12,27 @@ $start_time = microtime(true);
 $world = $argv[1];
 
 if (!$world) {
-	printf("No world supplied. \n Usage: $ php db_load_villages.php [world]");
+	printf("No world supplied. \n Usage: $ php db_load_players.php [world]");
 	exit();
 }
 
-$filepath = 'http://en' . $world. '.tribalwars.net/map/village.txt.gz';
-$local_filepath = '/code/twplan/beta/app/tmp/data/villages/en' . $world . '_village_data.txt';
+$filepath = 'http://en' . $world. '.tribalwars.net/map/player.txt.gz';
+$local_filepath = '/code/twplan/beta/app/tmp/data/players/en' . $world . '_player_data.txt';
 
 // Unzips the remote data file into an array
-$village_file = gzfile($filepath);
+$player_file = gzfile($filepath);
 
 // Preprocesses the csv to decode the village names and remove extraneous columns
-if (!$village_file) {
+if (!$player_file) {
 	printf("Error loading remote file %s\n", $filepath);
 	exit();
 }
 else {
-	printf("Loaded remote village data... \n");
+	printf("Loaded remote player data... \n");
 	$write_to = '';
-	for ($i = 0; $i < count($village_file); $i++) {
-		$line = explode(',', $village_file[$i]);
-		$write_to .= $line[0] . '>' . urldecode($line[1]) . '>' . $line[2] . '>' . $line[3] . '>' . $line[4] . "\n"; // Delimit using '>' because TW doesn't allow this character
+	for ($i = 0; $i < count($player_file); $i++) {
+		$line = explode(',', $player_file[$i]);
+		$write_to .= $line[0] . '>' . urldecode($line[1]) . '>' . $line[2] . "\n"; // Delimit using '>' because TW doesn't allow this character
 	}
 	printf("Processed data... \n");
 }
@@ -49,9 +49,9 @@ else {
 // Loads the database data from app/Config/database.php
 include('../../Config/database.php');
 $db_config = get_class_vars('DATABASE_CONFIG');
-$village_db_config = $db_config['villages'];
+$player_db_config = $db_config['players'];
 
-$mysqli = new mysqli($village_db_config['host'], $village_db_config['login'], $village_db_config['password'], $village_db_config['database']);
+$mysqli = new mysqli($player_db_config['host'], $player_db_config['login'], $player_db_config['password'], $player_db_config['database']);
 
 // Checks for connection error
 if ($mysqli->connect_errno) {
@@ -59,18 +59,16 @@ if ($mysqli->connect_errno) {
     exit();
 }
 else {
-	printf("Connected to database... \n");
+	printf("Connected to database...\n");
 }
 
 $create_query = "CREATE TABLE IF NOT EXISTS en{$world}
 			(
-				village_id INT NOT NULL,
-				name VARCHAR(100) NOT NULL,
-				x_coord INT NOT NULL,
-				y_coord INT NOT NULL,
 				player_id INT NOT NULL,
+				username VARCHAR(100) NOT NULL,
+				tribe_id INT NOT NULL,
 			PRIMARY KEY
-				(village_id)
+				(player_id)
 			)";
 
 if (!$mysqli->query($create_query)) {
@@ -86,15 +84,13 @@ $load_query = "LOAD DATA INFILE '{$local_filepath}' INTO TABLE en{$world}
 			FIELDS TERMINATED BY '>'
 			LINES TERMINATED BY '\n'
 			(
-				village_id,
-				name,
-				x_coord,
-				y_coord,
-				player_id
+				player_id,
+				username,
+				tribe_id
 			)";
 
 if (!$mysqli->query($load_query)) {
-    printf("Error loading village data with query \n %s \n", $load_query);
+    printf("Error loading player data with query \n %s \n", $load_query);
     printf("Error message: %s \n", $mysqli->error);
     exit();
 }
@@ -102,11 +98,22 @@ else {
 	printf("Parsed csv into mysql...\n");
 }
 
+$decode_query = "UPDATE en{$world} SET username = REPLACE(username, '+', ' ')";
+
+if (!$mysqli->query($decode_query)) {
+    printf("Error decoding player data with query \n %s \n", $decode_query);
+    printf("Error message: %s \n", $mysqli->error);
+    exit();
+}
+else {
+	printf("Decoded usernames...\n");
+}
+
 $mysqli->close();
 
 $end_time = microtime(true);
 
-printf("Database twp_villages successfully loaded village data for table en{$world}! \nElapsed time: %f \n", $end_time - $start_time);
+printf("Database twp_players successfully loaded player data for table en{$world}! \nElapsed time: %f \n", $end_time - $start_time);
 
 exit();
 
