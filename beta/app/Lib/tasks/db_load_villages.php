37acfed61c1542516c15d5645ec68d577b6e533f
Matt Dahl <matt.dahl.2013@gmail.com>
2013-08-31 16:57:19 -0700

    Updates data loading script for hawkhost use.

--- beta/app/Lib/tasks/db_load_villages.php
+++ beta/app/Lib/tasks/db_load_villages.php
@@ -1,57 +1,11 @@
 <?php
-/**
- * Intended for command line usage for local development. Loads village data into the local database. Example syntax for world 70:
- *
- * $ php db_load_villages.php 70
- */
-
 // Tracks execution time
 $start_time = microtime(true);
 
-// Grabs the second argument from the command line (the first argument is the script name)
-$world = $argv[1];
+session_start();
 
-if (!$world) {
-	printf("No world supplied.\nUsage: $ php db_load_villages.php [world]\n");
-	exit();
-}
-
-$filepath = 'http://en' . $world. '.tribalwars.net/map/village.txt.gz';
-$local_filepath = '/code/twplan/beta/app/tmp/data/villages/en' . $world . '_village_data.txt';
-
-// Unzips the remote data file into an array
-$village_file = gzfile($filepath);
-
-// Preprocesses the csv to decode the village names and remove extraneous columns
-if (!$village_file) {
-	printf("Error loading remote file %s\n", $filepath);
-	exit();
-}
-else {
-	printf("Loaded remote village data... \n");
-	$write_to = '';
-	for ($i = 0; $i < count($village_file); $i++) {
-		$line = explode(',', $village_file[$i]);
-		$write_to .= $line[0] . '>' . urldecode($line[1]) . '>' . $line[2] . '>' . $line[3] . '>' . $line[4] . "\n"; // Delimit using '>' because TW doesn't allow this character
-	}
-	printf("Processed data... \n");
-}
-
-// Pulls the processed csv file into the tmp folder
-if (!file_put_contents($local_filepath, $write_to)) {
-	printf("Error writing remote file %s to path %s\n", $filepath, $local_filepath);
-	exit();
-}
-else {
-	printf("Wrote data to tmp folder... \n");
-}
-
-// Loads the database data from app/Config/database.php
-include('../../Config/database.php');
-$db_config = get_class_vars('DATABASE_CONFIG');
-$village_db_config = $db_config['villages'];
-
-$mysqli = new mysqli($village_db_config['host'], $village_db_config['login'], $village_db_config['password'], $village_db_config['database']);
+// Connects to the database
+$mysqli = new mysqli("localhost", "twplanco", "@Uo54KjjtsLgxMx3NK2gd&!C", "twplanco_analytics");
 
 // Checks for connection error
 if ($mysqli->connect_errno) {
@@ -59,19 +13,115 @@ if ($mysqli->connect_errno) {
     exit();
 }
 else {
-	printf("Connected to database... \n");
+  printf("Connected to database... \n");
 }
 
+$worlds = array (
+  1=>array("world"=>19,"lastUpdated"=>""),
+  2=>array("world"=>30,"lastUpdated"=>""),
+  3=>array("world"=>38,"lastUpdated"=>""),
+  4=>array("world"=>42,"lastUpdated"=>""),
+  5=>array("world"=>46,"lastUpdated"=>""),
+  6=>array("world"=>48,"lastUpdated"=>""),
+  10=>array("world"=>56,"lastUpdated"=>""),
+  11=>array("world"=>57,"lastUpdated"=>""),
+  12=>array("world"=>58,"lastUpdated"=>""),
+  13=>array("world"=>59,"lastUpdated"=>""),
+  14=>array("world"=>60,"lastUpdated"=>""),
+  15=>array("world"=>61,"lastUpdated"=>""),
+  16=>array("world"=>63,"lastUpdated"=>""),
+  17=>array("world"=>64,"lastUpdated"=>""),
+  18=>array("world"=>65,"lastUpdated"=>""),
+  19=>array("world"=>66,"lastUpdated"=>""),
+  20=>array("world"=>67,"lastUpdated"=>""),
+  21=>array("world"=>68,"lastUpdated"=>""),
+  22=>array("world"=>69,"lastUpdated"=>"")
+);
+
+for ($i = 0; $i < count($worlds); $i++) {
+  $w = $worlds[$i]["world"];
+  $query = "SELECT `plans` FROM `plansPerWorld` WHERE `world` LIKE '$w'";
+  $result = $mysqli->query($query);
+  $resultArray = $result->fetch_array();
+  if ($resultArray["plans"] < 5) {
+    $worlds[$i]["lastUpdated"] = date("Y-m-d H:i:s");
+  }
+  else {
+    $query = "SELECT `dateTime` FROM `lastUpdated` WHERE `world` LIKE '$w'";
+    $result = $mysqli->query($query);
+    $resultArray = $result->fetch_array();
+    $worlds[$i]["lastUpdated"] = $resultArray["dateTime"];
+  }
+}
+
+function cmp_dates($a, $b) {
+    if (date($a["lastUpdated"]) == date($b["lastUpdated"]))
+      return 0;
+    else if (date($a["lastUpdated"]) < date($b["lastUpdated"]))
+      return -1;
+    else
+      return 1;
+}
+
+usort($worlds, "cmp_dates"); // organizes the array by last updated first
+
+$world = $worlds[0]["world"];
+
+mail("site@twplan.com", "TWplan Village Database Updated", "RefreshVillages W" . $world .  " Opened");
+
+if (!$world) {
+  //mail("site@twplan.com","TWplan Village Database Updated", "No world supplied");
+  printf("No world supplied\n");
+  exit();
+}
+else {
+  printf("Using world %d\n", $world);
+}
+
+set_time_limit(60); // in seconds; 1 min should be ample
+
+$filepath = 'http://en' . $world. '.tribalwars.net/map/village.txt.gz';
+$local_filepath = '/tmp/data/villages/en' . $world . '_village_data.txt';
+
+// Unzips the remote data file into an array
+$village_file = gzfile($filepath);
+
+// Preprocesses the csv to decode the village names and remove extraneous columns
+if (!$village_file) {
+  printf("Error loading remote file %s\n", $filepath);
+  exit();
+}
+else {
+  printf("Loaded remote village data... \n");
+  $write_to = '';
+  for ($i = 0; $i < count($village_file); $i++) {
+    $line = explode(',', $village_file[$i]);
+    $write_to .= $line[0] . '>' . urldecode($line[1]) . '>' . $line[2] . '>' . $line[3] . '>' . $line[4] . "\n"; // Delimit using '>' because TW doesn't allow this character
+  }
+  printf("Processed data... \n");
+}
+
+// Pulls the processed csv file into the tmp folder
+if (!file_put_contents($local_filepath, $write_to)) {
+  printf("Error writing remote file %s to path %s\n", $filepath, $local_filepath);
+  exit();
+}
+else {
+  printf("Wrote data to tmp folder... \n");
+}
+
+$mysqli->select_db("twplanco_villages");
+
 $create_query = "CREATE TABLE IF NOT EXISTS en{$world}
-			(
-				village_id INT NOT NULL,
-				village_name VARCHAR(100) NOT NULL,
-				x_coord INT NOT NULL,
-				y_coord INT NOT NULL,
-				player_id INT NOT NULL,
-			PRIMARY KEY
-				(village_id)
-			)";
+      (
+        village_id INT NOT NULL,
+        name VARCHAR(100) NOT NULL,
+        x_coord INT NOT NULL,
+        y_coord INT NOT NULL,
+        player_id INT NOT NULL,
+      PRIMARY KEY
+        (village_id)
+      )";
 
 if (!$mysqli->query($create_query)) {
     printf("Error creating table with query \n %s \n", $create_query);
@@ -79,19 +129,19 @@ if (!$mysqli->query($create_query)) {
     exit();
 }
 else {
-	printf("Created table (IF NOT EXISTS)...\n");
+  printf("Created table (IF NOT EXISTS)...\n");
 }
 
-$load_query = "LOAD DATA INFILE '{$local_filepath}' INTO TABLE en{$world}
-			FIELDS TERMINATED BY '>'
-			LINES TERMINATED BY '\n'
-			(
-				village_id,
-				village_name,
-				x_coord,
-				y_coord,
-				player_id
-			)";
+$load_query = "LOAD DATA LOCAL INFILE '{$local_filepath}' INTO TABLE en{$world}
+      FIELDS TERMINATED BY '>'
+      LINES TERMINATED BY '\n'
+      (
+        villageid,
+        name,
+        xcoord,
+        ycoord,
+        playerid
+      )";
 
 if (!$mysqli->query($load_query)) {
     printf("Error loading village data with query \n %s \n", $load_query);
@@ -99,29 +149,23 @@ if (!$mysqli->query($load_query)) {
     exit();
 }
 else {
-	printf("Parsed csv into mysql...\n");
+  printf("Parsed csv into mysql...\n");
 }
-
-$mysqli->select_db('twp_users');
-
-date_default_timezone_set("Europe/London");
-$now = date("Y-m-d H:i:s");
-$last_updated_query = "UPDATE worlds SET last_updated='{$now}' WHERE world_number = '{$world}'";
-
-if (!$mysqli->query($last_updated_query)) {
-	printf("Error updating last_updated data with query \n %s \n", $last_updated_query);
-	printf("Error message: %s \n", $mysqli->error);
-	exit();
-}
-else {
-	printf("Updated last_updated data...\n");
-}
-
-$mysqli->close();
 
 $end_time = microtime(true);
 
-printf("Database twp_villages successfully loaded village data for table en{$world}! \nElapsed time: %f \n", $end_time - $start_time);
+date_default_timezone_set("Europe/London");
+$currenttime = date("Y-m-d H:i:s");
+$message = "Database villages successfully loaded village data for table en{$world}! \nElapsed time:" . ($end_time - $start_time);
+mail("site@twplan.com", "TWplan Village Database Updated", $message);
+
+
+$mysqli->select_db("twplanco_analytics", $con);
+
+$updatetime = "UPDATE `analytics`.`lastUpdated` SET dateTime='$currenttime' WHERE `world` = '$world'";
+$mysqli->query($updatetime);
+
+$mysqli->close();
 
 exit();
 
