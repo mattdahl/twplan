f2a421b3abef6b60a730f89fd6222360c2bd98cb
Matt Dahl <matt.dahl.2013@gmail.com>
2013-09-09 20:44:01 -0700

    Removes old/ directory version control.

--- beta/old/scripts/loadjs.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo "<script src=\"scripts\hungarian6.js\"></script>
-    <script src=\"scripts\script.js\"></script>";
-
-?>
--- beta/old/scripts/updateworld.php
+++
@@ -1,7 +0,0 @@
-<?php
-  
-  session_start();
-  
-  $_SESSION["world"] = $_GET["world"];
-    
-?>
