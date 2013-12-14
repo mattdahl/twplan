619c00805ac421278cefad5a1c95fe7c67afc5e3
Matt Dahl <matt.dahl.2013@gmail.com>
2013-12-13 23:56:22 -0800

    Enables background highlighting for manually assigned targets. Nukes only so far.

--- beta/app/webroot/js/directives/manual_nuke_target_input.js
+++ beta/app/webroot/js/directives/manual_nuke_target_input.js
@@ -21,21 +21,38 @@ TWP.twplan.Directives.directive('manualNukeTargetInput', ['AutocompleteBuilder',
 				return Infinity;
 			};
 
+			toggle_manually_assigned_state = function (target) {
+				for (var i = 0; i < scope.targets.nukes.length; i++) {
+					if (scope.targets.nukes[i].x_coord === target.x_coord && scope.targets.nukes[i].y_coord === target.y_coord) {
+						scope.targets.nukes[i].manually_assigned = !scope.targets.nukes[i].manually_assigned;
+						break;
+					}
+				}
+			};
+
 			scope.update_manual_target = function (target) {
 				// If this update is replacing an existing manual target, put the existing one back in the scope.targets_in_plan array
 				if (scope.manual_target.scope) { // Checks to see if the manual_target variable holds a Target object and not merely a string
-					scope.targets_in_plan.nukes.push(new Target(
+					var relinquished_target = new Target(
 						scope.manual_target.scope,
 						scope.manual_target.x_coord,
 						scope.manual_target.y_coord,
 						scope.manual_target.continent,
 						scope.manual_target.attack_type
-					));
+					);
+					scope.targets_in_plan.nukes.push(relinquished_target);
+					scope.$apply(function () {
+						toggle_manually_assigned_state(relinquished_target);
+					});
 				}
 
 				// Remove the manual target from the scope.targets_in_plan.nukes array
 				scope.targets_in_plan.nukes.splice(index_of_target(target), 1);
 				scope.manual_target = target;
+
+				scope.$apply(function () {
+					toggle_manually_assigned_state(scope.manual_target);
+				});
 			};
 
 			scope.restore_manual_target = function () {
