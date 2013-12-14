32da418a7216f56d53783bde579e34a6536e55cf
Matt Dahl <matt.dahl.2013@gmail.com>
2013-12-13 23:36:49 -0800

    Transmutation of working nuke code to the noble and support counterparts.

--- beta/app/webroot/js/directives/manual_noble_target_input.js
+++ beta/app/webroot/js/directives/manual_noble_target_input.js
@@ -45,34 +45,50 @@ TWP.twplan.Directives.directive('manualNobleTargetInput', ['AutocompleteBuilder'
 		require: 'ngModel',
 		link: function (scope, elm, attrs, ctrl) {
 			scope.manual_target = null; // A Target object
-			scope.old_manual_target = null; // A Target object
 
 			// Initialize the autocomplete widget
 			AutocompleteBuilder.noble_autocomplete(elm);
 
-			// When focused, if there is currently a manual_target, keep a reference to the old coord value
-			elm.bind('focus', function () {
-				if (scope.manual_target) {
-					scope.old_manual_target = scope.manual_target;
+			// The autocomplete widget mangles our custom Target object, so it is necessary to check the coords to determine equality
+			index_of_target = function (target) {
+				if (!target) {
+					return Infinity;
 				}
-			});
+				for (var i = 0; i < scope.targets_in_plan.nobles.length; i++) {
+					if (scope.targets_in_plan.nobles[i].x_coord === target.x_coord && scope.targets_in_plan.nobles[i].y_coord === target.y_coord) {
+						return i;
+					}
+				}
+				return Infinity;
+			};
 
 			scope.update_manual_target = function (target) {
-				// Remove the manual target from the scope.targets_in_plan.nobles array
-				scope.targets_in_plan.nobles.splice(scope.targets_in_plan.nobles.indexOf(target), 1);
-				scope.manual_target = target;
-
-				// If this update is replacing an old manual target, put the old one back in the scope.targets_in_plan array
-				if (scope.old_manual_target) {
-					scope.targets_in_plan.nobles.push(scope.old_manual_target);
-					scope.old_manual_target = null;
+				// If this update is replacing an existing manual target, put the existing one back in the scope.targets_in_plan array
+				if (scope.manual_target.scope) { // Checks to see if the manual_target variable holds a Target object and not merely a string
+					scope.targets_in_plan.nobles.push(new Target(
+						scope.manual_target.scope,
+						scope.manual_target.x_coord,
+						scope.manual_target.y_coord,
+						scope.manual_target.continent,
+						scope.manual_target.attack_type
+					));
 				}
+
+				// Remove the manual target from the scope.targets_in_plan.nobles array
+				scope.targets_in_plan.nobles.splice(index_of_target(target), 1);
+				scope.manual_target = target;
 			};
 
 			scope.restore_manual_target = function () {
-				scope.manual_target = scope.old_manual_target;
-				scope.manual_target.label = scope.manual_target.x_coord + '|' + scope.manual_target.y_coord; // In case erased by ng-model update
-				scope.old_manual_target = null;
+				// Foces the input to revert to the old target coords
+				scope.$apply(function () {
+					if (scope.manual_target.scope) {
+						scope.manual_target.label = scope.manual_target.x_coord + '|' + scope.manual_target.y_coord;
+					}
+					else {
+						scope.manual_target.label = '';
+					}
+				});
 			};
 		}
 	};
--- beta/app/webroot/js/factories/autocomplete_builder.js
+++ beta/app/webroot/js/factories/autocomplete_builder.js
@@ -47,7 +47,7 @@ TWP.twplan.Factories.factory('AutocompleteBuilder', [function () {
 			});
 		},
 		noble_autocomplete: function (element) {
-			var scope = element.scope();
+			var scope = element.scope(); // Prototypically inherits from the StepTwoController $scope
 
 			$(element).autocomplete({
 				source: function (request, response) {
@@ -56,7 +56,7 @@ TWP.twplan.Factories.factory('AutocompleteBuilder', [function () {
 					for (var i = 0; i < scope.targets_in_plan.nobles.length; i++) {
 						var target = scope.targets_in_plan.nobles[i];
 						target.label = target.x_coord + '|' + target.y_coord;
-						if (target.label.indexOf(request.term)) { // If the target contains the coords being suggested by the user
+						if (target.label.indexOf(request.term) !== -1) { // If the target contains the coords being suggested by the user
 							noble_source.push(target); // Then add it the source array of suggestions to display to the user
 						}
 					}
@@ -65,18 +65,35 @@ TWP.twplan.Factories.factory('AutocompleteBuilder', [function () {
 				},
 				select: function (event, ui) {
 					element.scope().update_manual_target(ui.item); // When a new manual target is selected, pass it to the element's scope
+					element.scope().was_selected = true; // Sets a flag to prevent trigger of change() event when select() was
+					$(element).trigger('blur.autocomplete'); // Causes the change() callback to always be fired (left alone, behavior is unpredicable)
 				},
 				change: function (event, ui) {
-					// Prevents the user from inputting an invalid target
-					if (element.val().length && !ui.item) {
+					if (element.scope().was_selected) {
+						element.scope().was_selected = false;
+						return;
+					}
+					if (element.val().length) { // If the change is one that writes a new target *without* selecting from the dropdown...
+						// Check to see if the manual submission is valid
+						for (var i = 0; i < scope.targets_in_plan.nobles.length; i++) {
+							if (scope.targets_in_plan.nobles[i].x_coord + '|' + scope.targets_in_plan.nobles[i].y_coord === element.val()) {
+								// If so, update the manual target like normal
+								element.scope().update_manual_target(scope.targets_in_plan.nobles[i]);
+								return;
+							}
+						}
+						// If not, don't allow the manual submission
 						alert('You must make a valid target selection from the dropdown!');
 						element.scope().restore_manual_target();
+					}
+					else { // Otherwise, if the change is one that just deletes the current target, relinquish that manual target
+						element.scope().update_manual_target(null);
 					}
 				}
 			});
 		},
 		support_autocomplete: function (element) {
-			var scope = element.scope();
+			var scope = element.scope(); // Prototypically inherits from the StepTwoController $scope
 
 			$(element).autocomplete({
 				source: function (request, response) {
@@ -85,7 +102,7 @@ TWP.twplan.Factories.factory('AutocompleteBuilder', [function () {
 					for (var i = 0; i < scope.targets_in_plan.supports.length; i++) {
 						var target = scope.targets_in_plan.supports[i];
 						target.label = target.x_coord + '|' + target.y_coord;
-						if (target.label.indexOf(request.term)) { // If the target contains the coords being suggested by the user
+						if (target.label.indexOf(request.term) !== -1) { // If the target contains the coords being suggested by the user
 							support_source.push(target); // Then add it the source array of suggestions to display to the user
 						}
 					}
@@ -94,12 +111,29 @@ TWP.twplan.Factories.factory('AutocompleteBuilder', [function () {
 				},
 				select: function (event, ui) {
 					element.scope().update_manual_target(ui.item); // When a new manual target is selected, pass it to the element's scope
+					element.scope().was_selected = true; // Sets a flag to prevent trigger of change() event when select() was
+					$(element).trigger('blur.autocomplete'); // Causes the change() callback to always be fired (left alone, behavior is unpredicable)
 				},
 				change: function (event, ui) {
-					// Prevents the user from inputting an invalid target
-					if (element.val().length && !ui.item) {
+					if (element.scope().was_selected) {
+						element.scope().was_selected = false;
+						return;
+					}
+					if (element.val().length) { // If the change is one that writes a new target *without* selecting from the dropdown...
+						// Check to see if the manual submission is valid
+						for (var i = 0; i < scope.targets_in_plan.supports.length; i++) {
+							if (scope.targets_in_plan.supports[i].x_coord + '|' + scope.targets_in_plan.supports[i].y_coord === element.val()) {
+								// If so, update the manual target like normal
+								element.scope().update_manual_target(scope.targets_in_plan.supports[i]);
+								return;
+							}
+						}
+						// If not, don't allow the manual submission
 						alert('You must make a valid target selection from the dropdown!');
 						element.scope().restore_manual_target();
+					}
+					else { // Otherwise, if the change is one that just deletes the current target, relinquish that manual target
+						element.scope().update_manual_target(null);
 					}
 				}
 			});
