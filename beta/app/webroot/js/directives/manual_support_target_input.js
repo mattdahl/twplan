e8cbd6a0844fd3e2f068462bbac21df35a7e4c19
Matt Dahl <matt.dahl.2013@gmail.com>
2013-12-14 22:37:37 -0800

    Updates noble and support autocomplete directives with working nuke code.

--- beta/app/webroot/js/directives/manual_support_target_input.js
+++ beta/app/webroot/js/directives/manual_support_target_input.js
@@ -3,8 +3,6 @@ TWP.twplan.Directives.directive('manualSupportTargetInput', ['AutocompleteBuilde
 		scope: true,
 		require: 'ngModel',
 		link: function (scope, elm, attrs, ctrl) {
-			scope.manual_target = null; // A Target object
-
 			// Initialize the autocomplete widget
 			AutocompleteBuilder.support_autocomplete(elm);
 
@@ -21,31 +19,55 @@ TWP.twplan.Directives.directive('manualSupportTargetInput', ['AutocompleteBuilde
 				return Infinity;
 			};
 
+			scope.toggle_manually_assigned_state = function (target) {
+				for (var i = 0; i < scope.targets.supports.length; i++) {
+					if (scope.targets.supports[i].x_coord === target.x_coord && scope.targets.supports[i].y_coord === target.y_coord) {
+						scope.targets.supports[i].is_manually_assigned = !scope.targets.supports[i].is_manually_assigned;
+						break;
+					}
+				}
+			};
+
 			scope.update_manual_target = function (target) {
 				// If this update is replacing an existing manual target, put the existing one back in the scope.targets_in_plan array
-				if (scope.manual_target.scope) { // Checks to see if the manual_target variable holds a Target object and not merely a string
-					scope.targets_in_plan.supports.push(new Target(
-						scope.manual_target.scope,
-						scope.manual_target.x_coord,
-						scope.manual_target.y_coord,
-						scope.manual_target.continent,
-						scope.manual_target.attack_type
-					));
+				if (scope.village.manual_target.scope) { // Checks to see if the manual_target variable holds a Target object and not merely a string
+					var relinquished_target = new Target(
+						scope.village.manual_target.scope,
+						scope.village.manual_target.x_coord,
+						scope.village.manual_target.y_coord,
+						scope.village.manual_target.continent,
+						scope.village.manual_target.attack_type
+					);
+					scope.targets_in_plan.supports.push(relinquished_target);
+
+					// Toggle the state flag on both the relinquished target object and the new target object
+					scope.$apply(function () {
+						scope.toggle_manually_assigned_state(relinquished_target); // Relinquished target
+						if (target) { // Make sure the relinquished target isn't merely being replaced
+							scope.toggle_manually_assigned_state(target); // New target
+						}
+					});
+				}
+				else { // If this update is just adding a new target to a pristine state
+					// Toggle the state flag on both the relinquished target object
+					scope.$apply(function () {
+						scope.toggle_manually_assigned_state(target); // New target
+					});
 				}
 
 				// Remove the manual target from the scope.targets_in_plan.supports array
 				scope.targets_in_plan.supports.splice(scope.index_of_target(target), 1);
-				scope.manual_target = target;
+				scope.village.manual_target = target;
 			};
 
 			scope.restore_manual_target = function () {
 				// Foces the input to revert to the old target coords
 				scope.$apply(function () {
-					if (scope.manual_target.scope) {
-						scope.manual_target.label = scope.manual_target.x_coord + '|' + scope.manual_target.y_coord;
+					if (scope.village.manual_target.scope) {
+						scope.village.manual_target.label = scope.village.manual_target.x_coord + '|' + scope.village.manual_target.y_coord;
 					}
 					else {
-						scope.manual_target.label = '';
+						scope.village.manual_target.label = '';
 					}
 				});
 			};
